078ca6d8e321141c9523cfb54fa802f2a9ad8877
arzoo594 <arzooahmed0170609@gmail.com>
2025-12-11 23:08:40 +0600

    payment cancelled component created

---
+++ src/assets/Components/PaymentCancelled.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PaymentCancelled = () => {
+  return (
+    <div>
+      <p>this is a payment cancelled page</p>
+    </div>
+  );
+};
+
+export default PaymentCancelled;
